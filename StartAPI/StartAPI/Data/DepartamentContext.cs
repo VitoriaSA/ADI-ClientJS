8a18f462488b38f74c139b44d4759afe365bc5b2
vitorias <vitoriasimoes_2000@outlook.com>
2020-10-29 11:35:48 -0300

    Database Creation with migrations

---
+++ StartAPI/StartAPI/Data/DepartamentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StartAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartAPI.Data
+{
+    public class DepartamentContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=123456;Persist Security Info=False;User ID=admin;Initial Catalog=StartDB;Data Source=DESKTOP-T9EB9G3\\SQLEXPRESS");
+        }
+    }
+}
---
+++ StartAPI/StartAPI/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartAPI.Models
+{
+    public class Department
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        public string Initals { get; set; }
+        public List<Employee> Employees { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
