2180d45b67b8efcc46af04dcab73751a2bb13826
vitorias <vitoriasimoes_2000@outlook.com>
2020-11-04 18:10:46 -0300

    Swagger add

--- StartAPI/StartAPI/Startup.cs
+++ StartAPI/StartAPI/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Reflection;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,9 +13,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using StartAPI.Repository;
+
 
 namespace StartAPI
 {
@@ -39,11 +43,51 @@ namespace StartAPI
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
                 options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             });
+
+
+            ConfigureSwagger(services);
+
+
+        }
+
+        private static void ConfigureSwagger(IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Start API",
+                    Description = "A ASP.NET Core Web API for the Start project",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Vitória Simões",
+                        Email = "vitora.sacarvalho@gmail.com",
+                        Url = new Uri("https://github.com/VitoriaSA"),
+                    },
+
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
